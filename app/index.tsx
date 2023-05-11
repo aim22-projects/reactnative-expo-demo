a5dc0612e4fe8793219b785db3c2d10bc0d79a90
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-05-11 17:01:49 +0530

    Link to videos page is added

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
 import { Appbar, useTheme, withTheme } from 'react-native-paper';
+import ScreenPage from "../src/components/Page";
+import { Link } from "expo-router";
 export default function Index() {
   const theme = useTheme();
   return (
-    <View style={{ backgroundColor: theme.colors.background, flex: 1 }}>
+    <ScreenPage>
       <Appbar.Header>
         <Appbar.BackAction onPress={() => { }} />
         <Appbar.Content title="Title" />
         <Appbar.Action icon="calendar" onPress={() => { }} />
         <Appbar.Action icon="magnify" onPress={() => { }} />
       </Appbar.Header>
-    </View>
+      <Link href="videos/">Videos</Link>
+    </ScreenPage>
   );
 }
