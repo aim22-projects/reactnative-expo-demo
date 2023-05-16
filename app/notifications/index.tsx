039c42bacc0e62fef58f5195f4a80b2d378dad01
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-05-16 13:19:38 +0530

    Notifications page is created

---
+++ app/notifications/index.tsx
@@ -0,0 +1,40 @@
+import { Appbar, Button } from "react-native-paper";
+import ScreenPage from "../../src/components/Page";
+import { registerForPushNotificationsAsync, schedulePushNotification } from '../../src/services/local_notifications';
+import { useEffect, useRef, useState } from "react";
+
+import * as Notifications from 'expo-notifications';
+
+export default function NotificationsPage() {
+
+    const [expoPushToken, setExpoPushToken] = useState('');
+    const [notification, setNotification] = useState<Notifications.Notification>();
+    const notificationListener = useRef<any>();
+    const responseListener = useRef<any>();
+
+    useEffect(() => {
+        registerForPushNotificationsAsync().then(token => setExpoPushToken(token ?? ''));
+
+        notificationListener.current = Notifications.addNotificationReceivedListener(notification => {
+            setNotification(notification);
+        });
+
+        responseListener.current = Notifications.addNotificationResponseReceivedListener(response => {
+            console.log(response);
+        });
+
+        return () => {
+            Notifications.removeNotificationSubscription(notificationListener.current);
+            Notifications.removeNotificationSubscription(responseListener.current);
+        };
+    }, []);
+
+    return (
+        <ScreenPage>
+            <Appbar.Header elevated>
+                <Appbar.Content title="Videos" />
+            </Appbar.Header>
+            <Button onPress={async () => await schedulePushNotification()}>Press to schedule notification</Button>
+        </ScreenPage>
+    );
+}
