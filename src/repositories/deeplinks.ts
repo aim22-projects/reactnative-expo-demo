c7706c46b972779df0608131acd28eb805c71198
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-05-18 18:10:37 +0530

    deeplink repository is created

---
+++ src/repositories/deeplinks.ts
@@ -0,0 +1,7 @@
+export const deeplinksList = [
+    'https://myapp.project.com/videos/play?id=6',
+    'http://myapp.project.com/videos/play?id=6',
+    'https://myapp.project.com/videos',
+    'http://myapp.project.com/videos',
+    'myapp://videos',
+];
