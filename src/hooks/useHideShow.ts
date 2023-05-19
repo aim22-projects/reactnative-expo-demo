6ccd2b0bd8d8c9f5831b45dcf7b899c4e91f2cbd
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-05-19 12:45:20 +0530

    useHideShow hook is replaced with useVisibility hook

--- src/hooks/useHideShow.ts
+++
@@ -1,8 +0,0 @@
-import { useState } from "react";
-// TODO: testing is pending 
-export function useHideShow() {
-    const [visible, setVisible] = useState(false);
-    const showDialog = () => setVisible(true);
-    const hideDialog = () => setVisible(false);
-    return [visible, hideDialog, showDialog];
-}
---
+++ src/hooks/useVisibility.ts
@@ -0,0 +1,19 @@
+import { useState } from "react";
+/// return type for useVisibility hook
+type IUseVisibility = [
+    visible: boolean,
+    show: () => void,
+    hide: () => void,
+]
+
+// TODO: testing is pending 
+/**
+ * Return IUseVisibility, a stateful value (boolean) and functions to show (set to true) and hide (set to false)
+ * @returns IUseVisibility
+ */
+export function useVisibility(): IUseVisibility {
+    const [visible, setVisible] = useState(false);
+    const show = () => setVisible(true);
+    const hide = () => setVisible(false);
+    return [visible, show, hide];
+}
