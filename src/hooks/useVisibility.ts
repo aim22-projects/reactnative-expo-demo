f79309e6849c8689caa284cb070e1697c8b8e844
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-05-20 20:00:03 +0530

    Code documentation comments is selected code

--- src/hooks/useVisibility.ts
+++ src/hooks/useVisibility.ts
@@ -1,18 +1,27 @@
+// Importing the useState hook from React
 import { useState } from "react";
-/// return type for useVisibility hook
+
+// defining a custom return type that represents the stateful value (boolean) and functions to show (set to true) and hide (set to false)
 type IUseVisibility = [
-    visible: boolean,
-    show: () => void,
-    hide: () => void,
+    visible: boolean,   // boolean value that represents visibility state, true when visible, false when hidden
+    show: () => void,   // function that sets visibility state to true (shows the component)
+    hide: () => void,   // function that sets visibility state to false (hides the component)
 ]
 
 /**
- * Return IUseVisibility, a stateful value (boolean) and functions to show (set to true) and hide (set to false)
- * @returns IUseVisibility
+ * custom react hook that returns boolean visibility state and shows/hides functions.
+ * @returns IUseVisibility - a tuple with a boolean and two functions that change visibility state.
  */
 export function useVisibility(): IUseVisibility {
+    // Initializing a boolean variable 'visible' with a default state of 'false' using useState hook
     const [visible, setVisible] = useState(false);
+
+    // Defining a function called 'show' that sets the 'visible' state to true
     const show = () => setVisible(true);
+
+    // Defining a function called 'hide' that sets the 'visible' state to false
     const hide = () => setVisible(false);
+
+    // Returning the 'visible' state along with 'hide' and 'show' functions in a tuple
     return [visible, show, hide];
 }
