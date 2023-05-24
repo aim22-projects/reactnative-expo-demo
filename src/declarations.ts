8afeea4bf930b26b4fecdd2684edb450046069fc
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-05-24 20:30:37 +0530

    codebase is cleared

--- src/declarations.ts
+++
@@ -1,9 +0,0 @@
-/**
- * Declare modules to import non javascript and non typescript files
- */
-
-// images
-declare module "*.png";
-declare module "*.svg";
-declare module "*.jpeg";
-declare module "*.jpg";
