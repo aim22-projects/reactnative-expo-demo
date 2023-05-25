9c0adf25c3a655aec1d61c0f0eac16097dc80283
aasirmutwalli22 <aasirmutwalli22@gmail.com>
2023-05-25 08:27:54 +0530

    react native paper provider is injected in app

--- src/index.tsx
+++ src/index.tsx
@@ -2,8 +2,28 @@
  * @format
  */
 
-import {AppRegistry} from 'react-native';
+import { AppRegistry } from 'react-native';
 import App from './App';
-import {name as appName} from '../app.json';
+import { name as appName } from '../app.json';
+import { MD3LightTheme as DefaultTheme, MD3DarkTheme as DefaultDarkTheme, PaperProvider } from 'react-native-paper';
 
-AppRegistry.registerComponent(appName, () => App);
+const theme = {
+    ...DefaultTheme,
+    roundness: 2,
+    colors: {
+        ...DefaultTheme.colors,
+        // primary: 'tomato',
+        secondary: 'yellow',
+    },
+};
+
+
+function Main() {
+    return (
+        <PaperProvider theme={theme}>
+            <App />
+        </PaperProvider>
+    );
+}
+
+AppRegistry.registerComponent(appName, () => Main);
